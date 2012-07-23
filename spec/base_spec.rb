18d35c6fec6d409e33f8cd51423ee7be589c98b9
Anton Petrunich <anton.petrunich@gmail.com>
2012-07-23 14:49:11 +0300

    create spec_helper

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -1,12 +1,27 @@
 require 'spec_helper'
 describe HasSlug do
-  let(:subject) do
-    Class.new(ActiveRecord::Base) do
-      include HasSlug
-    end
-  end
+  let(:subject) { SluggedArticle.new }
 
-  it "should add slug attribute" do
-    subject.should respond_to? :slug
+  describe ".has_slug" do
+    it "should add slug attribute" do
+      subject.should respond_to(:slug)
+    end
+
+    it "should add .slug_config method" do
+      subject.class.should respond_to(:slug_config)
+    end
+
+    context "config" do
+      it "should raise ArgumentError if slug field not provided" do
+        lambda {
+          NotSluggedArticle.has_slug
+        }.should raise_exception(ArgumentError)
+
+      end
+
+      it "should correctly set model_class" do
+        subject.class.slug_config.model_class.should == subject.class
+      end
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,56 @@ require "bundler/setup"
 require "active_record"
 require 'rspec/mocks'
 RSpec::Mocks::setup(Object)
+
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+
+def setup_db
+  ActiveRecord::Schema.define(:version => 1) do
+    create_table :articles do |t|
+      t.string :title
+      t.string :text
+    end
+
+    create_table :categories do |t|
+      t.string :name
+      t.integer :weight
+    end
+  end
+end
+
+setup_db
+
+class SluggedArticle < ActiveRecord::Base
+  self.table_name = 'articles'
+  belongs_to :category
+
+  include HasSlug
+  has_slug :on => :title
+
+  def parent
+    category
+  end
+end
+
+class NotSluggedArticle < ActiveRecord::Base
+  self.table_name = 'articles'
+  belongs_to :category
+  include HasSlug
+
+  def parent
+    category
+  end
+end
+
+class Category < ActiveRecord::Base
+  has_many :slugged_articles
+  has_many :not_slugged_articles
+
+  include HasSlug
+  has_slug :on => :name
+
+
+  def children
+    articles
+  end
+end
